e2d4cdafce64dd5066a1f12b06cf0ca7d122f5c5
elseptiawan <septiawanelshandi@gmail.com>
2024-07-11 23:33:03 +0700

    install and setup swagger

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ const express = require('express');
 const { sendResponse } = require('./helpers/response');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const swaggerUi = require('swagger-ui-express');
+const swaggerSpecs = require('./swaggerConfig');
+const port = process.env.APP_PORT || 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -21,6 +23,8 @@ app.use('/books', bookRouter);
 app.use('/members', memberRouter);
 app.use('/borrows', borrowRouter);
 app.use('/returns', bookReturnRouter);
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpecs));
 
 app.use('/', (req, res) => {
     sendResponse(res, 404, 'Not Found');
