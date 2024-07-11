64163205f47bb86039b485ecb0c4223cad78408c
elseptiawan <septiawanelshandi@gmail.com>
2024-07-11 23:43:48 +0700

    create documentation for members endpoint

--- controllers/MemberController.js
+++ controllers/MemberController.js
@@ -1,6 +1,63 @@
 const {Member, BorrowingBook} = require('../models')
 const { sendResponse } = require('../helpers/response');
 
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Members:
+ *       type: object
+ *       required:
+ *         - id
+ *         - code
+ *         - name
+ *         - createdAt
+ *         - updatedAt
+ *       properties:
+ *         id:
+ *           type: integer
+ *           description: auto increment unique identifier
+ *         code:
+ *           type: string
+ *           description: The code of the member
+ *         name:
+ *           type: string
+ *           description: The name of the member
+ *         total_borrowed:
+ *           type: integer
+ *           description: The total borrowed books
+ *       example:
+ *         id: 1
+ *         code: M001
+ *         name: Angga
+ *         createdAt: 2021-01-01
+ *         updatedAt: 2021-01-01
+ *         total_borrowed: 2
+ */
+
+/**
+ * @swagger
+ * tags:
+ *   name: Members
+ *   description: Members Endpoint
+ */
+
+/**
+ * @swagger
+ * /members:
+ *   get:
+ *     summary: Returns the list of the members and the total borrowed books
+ *     tags: [Members]
+ *     responses:
+ *       200:
+ *         description: The list of the members
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/Members'
+ */
 exports.getAll = async (req, res) => {
     try{
         const members = await Member.findAll({
